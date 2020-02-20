c3d7fd0fdf138904f2fb9b96b8c6730373308122
K1DV5 <kidusadugna@gmail.com>
2020-02-20 18:12:40 +0300

    update nvim setup.ps1 and gitignore

--- nvim/setup.ps1
+++ nvim/setup.ps1
@@ -4,6 +4,5 @@ $deployPath = $HOME + "\AppData\Local\nvim"
 
 cmd /c mklink /D $deployPath\after 		        $PWD\after
 cmd /c mklink /D $deployPath\plugin 	        $PWD\plugin
-cmd /c mklink    $deployPath\coc-settings.json 	$PWD\coc-settings.json
 cmd /c mklink    $deployPath\ginit.vim 		    $PWD\ginit.vim
 cmd /c mklink    $deployPath\init.vim 		    $PWD\init.vim

0484255ad042fd8b08ef5efb1093c7fcabc6d417
K1DV5 <kidusadugna@gmail.com>
2020-07-05 15:27:21 +0300

    nvim: make only one softlink

--- nvim/setup.ps1
+++
@@ -1,9 +0,0 @@
-# cd to the nvim dir and run the followig commands
-
-$deployPath = $HOME + "\AppData\Local\nvim"
-
-cmd /c mklink /D $deployPath\after 		        $PWD\after
-cmd /c mklink /D $deployPath\plugin 	        $PWD\plugin
-cmd /c mklink /D $deployPath\lua 	            $PWD\lua
-cmd /c mklink    $deployPath\ginit.vim 		    $PWD\ginit.vim
-cmd /c mklink    $deployPath\init.vim 		    $PWD\init.vim

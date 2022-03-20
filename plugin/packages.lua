a224075a38f3ad0d57318d047a73cc58492dd064
K1DV5 <kidusadugna@gmail.com>
2022-03-20 12:07:26 +0300

    use ssh url for paq

--- plugin/packages.lua
+++ plugin/packages.lua
@@ -3,7 +3,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+  fn.system({'git', 'clone', '--depth=1', 'git@github.com:savq/paq-nvim.git', install_path})
 end
 
 -- setup packages only if they exist, to not prevent loading other parts
